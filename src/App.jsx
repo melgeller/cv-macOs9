c8bef5633417f3f84a0346e5ec1f98113995fd71
melgeller <melgeller.foto@gmail.com>
2022-12-08 10:26:18 +0100

    bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import "./App.scss";
 import Navbar from "./components/Navbar.jsx";
 import Draggable from "react-draggable";
+import { useState } from "react";
 
 function App() {
+  const open = require('./imgs/barraclosed.png');
+  const closed = require('./imgs/barraopen.png');
+  const [img, setImg] = useState(false);
+
+  const imgChangeHandler = () => {
+    if(!img) {
+      setImg(true);
+    }else{
+      setImg(false)
+    }
+  };
   return (
     <div className='desktop'>
       <Navbar></Navbar>
@@ -10,37 +22,37 @@ function App() {
         <div className='orden'>
           <Draggable>
             <div className='carpetPic'>
-              <img src='../1.png' alt='' />
+              <input type="image" name="hd" id="hd" src="../1.png" alt="hd"/>
               <p>MacintoshHD</p>
             </div>
           </Draggable>
           <Draggable>
             <div className='carpetPic'>
-              <img src='../77.png' alt='' />
+            <input type="image" name="pp" id="pp" src="../77.png" alt="pp"/>
               <p>Profile Pic</p>
             </div>
           </Draggable>
           <Draggable>
             <div className='carpetPic'>
-              <img src='../11.png' alt='' />
+            <input type="image" name="about" id="about" src="../11.png" alt="about"/>
               <p>About Me</p>
             </div>
           </Draggable>
           <Draggable>
             <div className='carpetPic'>
-              <img src='../58.png' alt='' />
+            <input type="image" name="tools" id="tools" src="../58.png" alt="tools"/>
               <p>Tools</p>
             </div>
           </Draggable>
           <Draggable>
             <div className='carpetPic'>
-              <img src='../44.png' alt='' />
+            <input type="image" name="edu" id="edu" src="../44.png" alt="edu"/>
               <p>Education</p>
             </div>
           </Draggable>
           <Draggable>
             <div className='carpetPic'>
-              <img src='../62.png' alt='' />
+            <input type="image" name="work" id="work" src="../62.png" alt="work"/>
               <p>Work Exp</p>
             </div>
           </Draggable>
@@ -48,16 +60,18 @@ function App() {
         <div className="trash">
             <Draggable>
               <div className='carpetPic'>
-                <img src='../6.png' alt='' />
+              <input type="image" name="trash" id="trash" src="../6.png" alt="trash"/>
                 <p>Trash</p>
               </div>
             </Draggable>
           </div>
       </div>
       <div className="barra">
-      <img src="../barraopen.png" alt=""/>
+      <img src={!img ? open : closed } alt="" onClick={imgChangeHandler}/>
       </div>
+      
     </div>
+    
   );
 }
 
---
+++ src/components/ProfilePic.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ProfilePic = () => {
+  return (
+    <div>
+        <img src="../profilepic.png" alt=""/>
+    </div>
+  )
+}
